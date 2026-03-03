cbd379b06446caa134c14181b8a9d48cbfe6d14f
brunoandrade2 <ndbs11118@gmail.com>
2026-03-03 11:45:13 -0300

    aula 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
-function App () {
-    return(
-      <div>
-        <h1>Aula de React com Vite JS  </h1>
-        <h2>Estilzação CSS</h2>
-        <p>eu nao fiz nada</p>
-      </div>
+import { Routes, Route } from 'react-router-dom'
 
-    )
-  }
+import Home from './pages/home'
+import Sobre from './pages/sobre'
 
-  export default App;
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sobre" element={<Sobre />} />
+    </Routes>
+  )
+}
+
+export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+function Home () {
+    return (
+ <div>
+<h1>sobre aula de react com Vite JS</h1>
+ <h3>Estilizacao css</h3>
+<p>eu nao fiz nada</p>
+</div>
+    )
+
+}
+
+export default Home;
