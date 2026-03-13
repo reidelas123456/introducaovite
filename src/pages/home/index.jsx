bf029d0925d8cdceec03cf37b16ebf4f690ec392
brunoandrade2 <ndbs11118@gmail.com>
2026-03-13 14:40:13 -0300

    aula 5

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,22 @@
-function Home () {
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router';
+
+function Home() {
+
+    const navigate = useNavigate();
+
     return (
- <>
-<h1>sobre aula de react com Vite JS</h1>
+        <div>
+            <h1>sobre aula de react com Vite JS</h1>
 
-</>
-    )
+            <button onClick={() => navigate('/exemplo/1')}>
+                exemplo 1
+            </button>
 
+            <Link to="/exemplo/2">Exemplo 2 </Link>
+
+        </div>
+    );
 }
 
 export default Home;
