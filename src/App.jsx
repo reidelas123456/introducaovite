01bf0ced2136d718a3df94267f0c3ce308153392
brunoandrade2 <ndbs11118@gmail.com>
2026-03-24 11:43:16 -0300

    aula 24/03

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/home'
 import Exemplos01 from './pages/exemplos/ex-01'
 import Exemplos02 from './pages/exemplos/ex-02'
 import Atividade01 from './pages/atividades/atv-01'
+import Exemplos03 from './pages/exemplos/ex-03'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/exemplo/1" element={<Exemplos01 />} />
       <Route path="/exemplo/2" element={<Exemplos02 />} />
       <Route path="/atividades/1" element={<Atividade01 />} />
+      <Route path="/exemplo/3" element={<Exemplos03 />} />
     </Routes>
   )
 }
